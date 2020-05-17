0211a6df40cb39f926ee5d11eb9d6ad4c76f86c3
Eduardo Sorensen <eduardo.sorensen@gmail.com>
2020-05-17 15:08:37 -0300

    #1 fix unit test

--- test/app/modules/home/home_page_test.dart
+++ test/app/modules/home/home_page_test.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
-
+import 'package:okr_os/app/app_module.dart';
+import 'package:okr_os/app/modules/home/home_module.dart';
 import 'package:okr_os/app/modules/home/home_page.dart';
 
 main() {
+  initModule(AppModule());
+  initModule(HomeModule());
   testWidgets('HomePage has title', (tester) async {
     await tester.pumpWidget(buildTestableWidget(HomePage(title: 'Home')));
     final titleFinder = find.text('Home');

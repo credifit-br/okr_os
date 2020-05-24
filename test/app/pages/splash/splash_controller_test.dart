a2d314961b5a2d0e3afd5f7f9964a0354ea59b39
Eduardo Sorensen <eduardo.sorensen@gmail.com>
2020-05-24 15:22:51 -0300

    #8 clean unnused imports

--- test/app/pages/splash/splash_controller_test.dart
+++ test/app/pages/splash/splash_controller_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:okr_os/app/pages/splash/splash_controller.dart';
 import 'package:okr_os/app/app_module.dart';
 
 void main() {

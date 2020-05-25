9e16b7774f09a4214a36aac17738e937b5ba23fc
Eduardo Sorensen <eduardo.sorensen@gmail.com>
2020-05-25 14:35:38 -0300

    #6 add unit tests

--- test/app/modules/home/home_controller_test.dart
+++ test/app/modules/home/home_controller_test.dart
@@ -1,26 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:okr_os/app/app_module.dart';
 
 import 'package:okr_os/app/modules/home/home_controller.dart';
 import 'package:okr_os/app/modules/home/home_module.dart';
 
+class MockHomeController extends Mock implements HomeController {}
+
 void main() {
-  initModule(HomeModule());
+  TestWidgetsFlutterBinding.ensureInitialized();
   HomeController home;
-  //
+  MockHomeController homeMock;
+
   setUp(() {
+    Modular.init(AppModule());
+    Modular.init(HomeModule());
     home = HomeModule.to.get<HomeController>();
+    homeMock = MockHomeController();
   });
 
   group('HomeController Test', () {
     test("First Test", () {
-      expect(home, isInstanceOf<HomeController>());
+      expect(home, isA<HomeController>());
     });
 
-    test("Set Value", () {
-      expect(home.value, equals(0));
-      home.increment();
-      expect(home.value, equals(1));
+    test("logoff", () async {
+      expect((await homeMock.logoff()), isNull);
     });
   });
 }
--- test/app/shared/auth/auth_controller_test.dart
+++ test/app/shared/auth/auth_controller_test.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -5,6 +6,7 @@ import 'package:okr_os/app/shared/auth/auth_controller.dart';
 import 'package:okr_os/app/app_module.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
   initModule(AppModule());
   AuthController auth;
 
